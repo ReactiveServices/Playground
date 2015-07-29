3056d9b67c93fe60bb44bdcf4cc85c44bc30110c
Rafael Romão <rafaelromao@gmail.com>
2015-07-29 19:40:15 -0300

    Initial commit

---
+++ ReactiveServices/Demo/HelloWorld.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using ReactiveServices.ComputationalUnit.Work;
+
+namespace Demo
+{
+    public class HelloWorldWorker : Worker
+    {
+        protected override bool TryExecute()
+        {
+            Console.WriteLine("Hello World!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Bye!");
+            return true;
+        }
+
+        protected override void Complete()
+        {
+            
+        }
+
+        protected override void Timeout()
+        {
+        
+        }
+
+        protected override void Fail()
+        {
+        }
+    }
+
+    public class HelloWorldJob : Job
+    {
+    }
+}
---
+++ ReactiveServices/Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bootstrap you Reactive Services application according to the Bootstrap.config file
+            ReactiveServices.Application.Bootstrap.Run();
+        }
+    }
+}
